5c7c2b6e7e808f0f041b181e0354db5e6c40e816
HITESHKUMAR PATEL <40137107+hiteshpatel0926@users.noreply.github.com>
2026-06-05 22:30:09 +0530

    Font Changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/800.css";
+
 import "./index.css";
 
 import App from "./App";
 
-import {
-  AuthProvider
-} from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
 
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <App />
-  </AuthProvider>
+  </AuthProvider>,
 );
